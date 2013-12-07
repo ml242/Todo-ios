f64981366dafa5b0e04e13ebc402db7601643cfe
Matt Lucas <mattlucas@gmail.com>
2013-12-07 15:40:02 -0500

    making changes

--- Todo-iOS/mslViewController.h
+++ Todo-iOS/mslViewController.h
@@ -5,9 +5,16 @@
 //  Created by Matt Lucas on 12/7/13.
 //  Copyright (c) 2013 Matt Lucas. All rights reserved.
 //
+//  This is my super cool algorithm
 
 #import <UIKit/UIKit.h>
 
+
 @interface mslViewController : UIViewController
+{
+    IBOutlet UITextField *myTextField;
+    IBOutlet UILabel *myLabel;
+}
+- (IBAction)clickMeButtonTapped;
 
 @end
--- Todo-iOS/mslViewController.m
+++ Todo-iOS/mslViewController.m
@@ -12,6 +12,9 @@
 
 @end
 
+//IBOutlet *myTextField;
+//IBOutlet *myLabel;
+
 @implementation mslViewController
 
 - (void)viewDidLoad
@@ -24,6 +27,43 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)clickMeButtonTapped
+{
+    NSString *textFieldContents = myTextField.text;
+
+    NSArray *myArray = @[@"Matt",@"Joe",@"Avi",@"Jane"];
+    NSMutableArray *myMutableArray = [[NSMutableArray alloc] init];
+
+    [myMutableArray addObject:(@"Matt")];
+    [myMutableArray addObject:(@"Joe")];
+    [myMutableArray addObject:(@"Avi")];
+    [myMutableArray addObject:(@"Jane")];
+    
+    [myMutableArray removeObject:@"Matt"];
+    
+    for (NSInteger i=0; i<[myMutableArray count]; i++) {
+        NSLog(@"%@",myMutableArray[i]);
+
+    }
+    
+    if ([textFieldContents isEqualToString:@"Matt"]) {
+        NSLog(@"Nice Name");
+    } else if ([textFieldContents isEqualToString:@"Avi"]) {
+        NSLog(@"Horrible Name");
+    } else {
+        NSLog(@"Not the best name, there");
+    }
+
+    for (NSInteger i=0; i<10 ; i++){
+        NSLog(@"Text Field text: %@",myTextField.text);
+    }
+    
+    myLabel.text = textFieldContents;
+
+//    NSNumber *aNumber =@7;
+//    NSLog(@"Text Field text: %@",myTextField.text);
 }
 
 @end
